df7f112fb88fd27ff56bf45547558094312f66f3
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-11-21 13:19:25 +0100

    Deploy on Goerli

--- rendus/03_projet/truffle/truffle-config.js
+++ rendus/03_projet/truffle/truffle-config.js
@@ -9,12 +9,12 @@ module.exports = {
      port: 8545,
      network_id: "*",
     },
-    // goerli: {
-    //   provider: function() {
-    //     return new HDWalletProvider({ mnemonic: {phrase:`${process.env.MNEMONIC}`}, providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_ID}` })
-    //   },
-    //   network_id: 5,
-    // },
+    goerli: {
+      provider: function() {
+        return new HDWalletProvider({ mnemonic: {phrase:`${process.env.MNEMONIC}`}, providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_ID}` })
+      },
+      network_id: 5,
+    },
   },
   
   mocha: {
