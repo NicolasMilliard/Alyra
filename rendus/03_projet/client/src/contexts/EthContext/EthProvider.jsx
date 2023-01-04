b4f2426525eaa6c6065e66bbee2f2735030075e9
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-01-04 09:47:24 +0100

    Handle browsers without MetaMask installed

--- rendus/03_projet/client/src/contexts/EthContext/EthProvider.jsx
+++ rendus/03_projet/client/src/contexts/EthContext/EthProvider.jsx
@@ -7,27 +7,26 @@ function EthProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [currentAccount, setCurrentAccount] = useState("");
 
-  const init = useCallback(
-    async artifact => {
-      if (artifact) {
-        const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-        const accounts = await web3.eth.requestAccounts();
-        setCurrentAccount(accounts[0]);
-        const networkID = await web3.eth.net.getId();
-        const { abi } = artifact;
-        let address, contract;
-        try {
-          address = artifact.networks[networkID].address;
-          contract = new web3.eth.Contract(abi, address);
-        } catch (err) {
-          console.error(err);
-        }
-        dispatch({
-          type: actions.init,
-          data: { artifact, web3, accounts, networkID, contract }
-        });
+  const init = useCallback(async (artifact) => {
+    if (artifact) {
+      const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+      const accounts = await web3.eth.requestAccounts();
+      setCurrentAccount(accounts[0]);
+      const networkID = await web3.eth.net.getId();
+      const { abi } = artifact;
+      let address, contract;
+      try {
+        address = artifact.networks[networkID].address;
+        contract = new web3.eth.Contract(abi, address);
+      } catch (err) {
+        console.error(err);
       }
-    }, []);
+      dispatch({
+        type: actions.init,
+        data: { artifact, web3, accounts, networkID, contract },
+      });
+    }
+  }, []);
 
   useEffect(() => {
     const tryInit = async () => {
@@ -48,29 +47,38 @@ function EthProvider({ children }) {
       init(state.artifact);
     };
 
-    events.forEach(e => window.ethereum.on(e, handleChange));
+    if (window.ethereum != undefined) {
+      events.forEach((e) => window.ethereum.on(e, handleChange));
+    }
+
     return () => {
-      events.forEach(e => window.ethereum.removeListener(e, handleChange));
+      events.forEach((e) => window.ethereum.removeListener(e, handleChange));
     };
   }, [init, state.artifact]);
 
-  const connectWallet = async() => {
-    const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-    const accounts = await web3.eth.requestAccounts();
-    setCurrentAccount(accounts[0]);
-  }
+  const connectWallet = async () => {
+    if (window.ethereum == undefined) {
+      alert("Please install MetaMask");
+    } else {
+      const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+      const accounts = await web3.eth.requestAccounts();
+      setCurrentAccount(accounts[0]);
+    }
+  };
 
   useEffect(() => {
     connectWallet();
   });
 
   return (
-    <EthContext.Provider value={{
-      state,
-      dispatch,
-      connectWallet,
-      currentAccount
-    }}>
+    <EthContext.Provider
+      value={{
+        state,
+        dispatch,
+        connectWallet,
+        currentAccount,
+      }}
+    >
       {children}
     </EthContext.Provider>
   );
